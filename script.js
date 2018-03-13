2a1ef03cc890a2de9ed102aa46a5aabe76227259
Frank Ruiz <sanreynaldo@gmail.com>
2018-03-13 16:39:38 -0400

    Clean code

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const weather_api = 'http://api.openweathermap.org/data/2.5/weather?q=miami,us&APPID=ca10a83000b46a985e0f69e06274f847';
 
 window.addEventListener('load', function() {
-
 });
 
 function getWeatherData() {
   const data = JSON.parse(this.responseText);
   console.dir(data);
-
   const sunrise = data.sys.sunrise;
   const sunset = data.sys.sunset;
   const currentTime = new Date().getTime()/1000;
   const timeSinceSunrise = currentTime - sunrise;
   const noonTime = sunrise + ((sunset - sunrise) / 2);
   console.log(noonTime);
-
-
 }
 
 const connection = new XMLHttpRequest();
